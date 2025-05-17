356a179cdfd91495fb24483056564814ad3d9a30
Sebastian Cerviño <cervisebas@gmail.com>
2025-05-17 19:53:10 -0300

    fix: ref attribute stackedlayout

--- src/components/StackedLayout/interfaces/StackedLayoutProps.ts
+++ src/components/StackedLayout/interfaces/StackedLayoutProps.ts
@@ -3,6 +3,7 @@ import { RNStackedLayout } from "../scripts/RNStackedLayout";
 import { ViewBaseProps } from "../../../interfaces/ViewBaseProps";
 
 export interface StackedLayoutProps extends ViewBaseProps<RNStackedLayout> {
+  ref?: React.Ref<RNStackedLayout>;
   currentIndex?: number;
   currentName?: string;
   children?: React.ReactNode;
