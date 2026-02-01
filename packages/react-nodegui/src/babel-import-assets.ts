ef197b6e96c18bbb82c8b07d75540609f30b1224
Sebastian Cerviño <cervisebas@gmail.com>
2026-02-01 17:14:54 -0300

    feat: se movio el plugin de babel a la libreria react-nodegui

---
+++ packages/react-nodegui/src/babel-import-assets.ts
@@ -0,0 +1,66 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export default function ({ types: t }: any) {
+  return {
+    visitor: {
+      Program: {
+        enter(progPath: any) {
+          let usesAssets = false;
+          let hasPathImport = false;
+
+          progPath.traverse({
+            ImportDeclaration(pathNode: any) {
+              const source = pathNode.node.source.value;
+
+              if (/\.(png|jpe?g|gif|svg|bmp|otf)$/i.test(source)) {
+                usesAssets = true;
+                
+                const specifiers = pathNode.node.specifiers;
+                if (specifiers.length === 0) {
+                  return;
+                }
+
+                const localName = specifiers[0].local.name;
+
+                const replacement = t.variableDeclaration("const", [
+                  t.variableDeclarator(
+                    t.identifier(localName),
+                    t.callExpression(
+                      t.memberExpression(t.identifier("path"), t.identifier("resolve")),
+                      [
+                        t.identifier("__dirname"),
+                        t.callExpression(t.identifier("require"), [t.stringLiteral(source)])
+                      ]
+                    )
+                  )
+                ]);
+
+                pathNode.replaceWith(replacement);
+              }
+
+              if (source === 'path') {
+                hasPathImport = true;
+              }
+            },
+            VariableDeclarator(pathNode: any) {
+              if (t.isIdentifier(pathNode.node.id, { name: "path" })) {
+                hasPathImport = true;
+              }
+            }
+          });
+
+          if (usesAssets && !hasPathImport) {
+            progPath.unshiftContainer(
+              "body",
+              t.variableDeclaration("const", [
+                t.variableDeclarator(
+                  t.identifier("path"),
+                  t.callExpression(t.identifier("require"), [t.stringLiteral("path")])
+                )
+              ]),
+            );
+          }
+        }
+      }
+    }
+  };
+};
